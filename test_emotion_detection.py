4100f4937c42fe6c4f8c5c89d8ef4893a402137b
nwmarlin <nwmarlin@gmail.com>
2025-06-20 00:19:19 -0400

    working unit test

--- test_emotion_detection.py
+++ test_emotion_detection.py
@@ -26,5 +26,16 @@ class TestEmotionDetector(unittest.TestCase):
         # Test Case for joy statment
         result_1 = emotion_detector("I am glad this happened")
         self.assertEqual(result_1['dominant_emotion'], "joy")
-
-    unittest.main()
+        # Test Case for anger statement
+        result_2 = emotion_detector("I am really mad about this")
+        self.assertEqual(result_2['dominant_emotion'], "anger")
+        # Test Case for disgust statment
+        result_3 = emotion_detector("I fell disgusted just about hearing this")
+        self.assertEqual(result_3['dominant_emotion'], "disgust")
+        # Test Case for sadness statement
+        result_4 = emotion_detector("I am so sad about this")
+        self.assertEqual(result_4['dominant_emotion'], "sadness")
+        # Test case for fear statement
+        result_5 = emotion_detector("I am really afraid this will happen")
+        self.assertEqual(result_5['dominant_emotion'], "fear")
+unittest.main()
