86b933798bf313bf9cb947b7d2bd8b8a21ba9d3e
nwmarlin <nwmarlin@gmail.com>
2025-06-20 00:10:51 -0400

    First working version of emotion detector

---
+++ test_emotion_detection.py
@@ -0,0 +1,30 @@
+"""
+The purpose of the module is to run unit tests
+for the function emotion_detector
+"""
+# Import the required libraries
+import unittest
+from EmotionDetection.emotion_detection import emotion_detector
+
+# Create the class for performing the unittesting
+class TestEmotionDetector(unittest.TestCase):
+    # Define the function for testing
+    def test_emotion_detector(self):
+        """
+        The purpose of this function is to provide 5 tests:
+            Statement: I am glad this happened
+            Dominant Emotion: joy
+            Statment: I am really mad about this
+            Dominant Emotion: andger
+            Statment: I fell disgusted just hearung about this
+            Dominant Emotion: disgust
+            Statement: I am so sad about this
+            Dominant Emotion: sadness
+            Statment: I am really afraid that this will happen
+            Dominant Emotion: fear
+        """
+        # Test Case for joy statment
+        result_1 = emotion_detector("I am glad this happened")
+        self.assertEqual(result_1['dominant_emotion'], "joy")
+
+    unittest.main()
